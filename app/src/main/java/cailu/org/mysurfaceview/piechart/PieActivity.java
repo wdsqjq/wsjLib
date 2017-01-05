2191dbb7d86955e2ceef3ca856512f2c92af8e42
wangsj <1521192337@qq.com>
2017-01-05 17:55:52 +0800

    饼状图

---
+++ app/src/main/java/cailu/org/mysurfaceview/piechart/PieActivity.java
@@ -0,0 +1,47 @@
+package cailu.org.mysurfaceview.piechart;
+
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+
+import java.util.ArrayList;
+
+import cailu.org.mysurfaceview.R;
+
+import static cailu.org.mysurfaceview.R.id.pie;
+
+public class PieActivity extends AppCompatActivity {
+
+    PieView pieView;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_pie);
+
+        pieView= (PieView) findViewById(pie);
+
+        final ArrayList<PieData> mData=new ArrayList<>();
+        mData.add(new PieData("张三",30));
+        mData.add(new PieData("李四",40));
+        mData.add(new PieData("王五",50));
+        pieView.setData(mData);
+
+        new Thread(new Runnable() {
+            @Override
+            public void run() {
+                try {
+                    Thread.sleep(5000);
+                    mData.add(new PieData("赵6",60));
+                    runOnUiThread(new Runnable() {
+                        @Override
+                        public void run() {
+                            pieView.setData(mData);
+                        }
+                    });
+                } catch (InterruptedException e) {
+                    e.printStackTrace();
+                }
+            }
+        }).start();
+    }
+}
