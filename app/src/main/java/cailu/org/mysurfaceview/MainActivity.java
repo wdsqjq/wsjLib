3951e5883fe7b0acaa03935e59013e282ecc4b3c
wangsj <1521192337@qq.com>
2017-01-08 15:15:04 +0800

    微信雷达扫描

--- app/src/main/java/cailu/org/mysurfaceview/MainActivity.java
+++ app/src/main/java/cailu/org/mysurfaceview/MainActivity.java
@@ -1,16 +1,21 @@
 package cailu.org.mysurfaceview;
 
+import android.app.Activity;
 import android.os.Bundle;
-import android.support.v7.app.AppCompatActivity;
 
-public class MainActivity extends AppCompatActivity {
+public class MainActivity extends Activity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
+//        requestWindowFeature(Window.FEATURE_NO_TITLE);
         super.onCreate(savedInstanceState);
 //        setContentView(new FreeFall(this));
-        RadarView radarView=new RadarView(this);
-//        radarView.setMainPaintColor(Color.BLUE);
-        setContentView(radarView);
+
+//        // 蜘蛛网图
+//        RadarView radarView=new RadarView(this);
+//        setContentView(radarView);
+
+        //微信雷达搜索
+        setContentView(R.layout.activity_main2);
     }
 }
