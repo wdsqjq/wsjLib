ab88384f1b71fdea988c16416390b070863e15e8
shiju.wang <shiju.wang@wheatek.com>
2017-11-02 18:43:58 +0800

    hotman加载

---
+++ hotmanloading/src/main/java/com/example/wsj/hotmanloading/MainActivity.java
@@ -0,0 +1,14 @@
+package com.example.wsj.hotmanloading;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+//        setContentView(R.layout.activity_main);
+        setContentView(new HotManLoading(this));
+    }
+}
