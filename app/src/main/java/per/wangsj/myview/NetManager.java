f2112c46d6cd41c394a93767889831e15c5c0d9f
shiju.wang <shiju.wang@wheatek.com>
2018-07-16 16:28:55 +0800

    fix bug

--- app/src/main/java/per/wangsj/myview/NetManager.java
+++ app/src/main/java/per/wangsj/myview/NetManager.java
@@ -1,31 +1,30 @@
 package per.wangsj.myview;
 
 
-import per.wsj.commonlib.net.HttpUtil;
-
 /**
  * Created by shiju.wang on 2018/2/28.
  */
 
-public class NetManager extends HttpUtil {
-    private static final NetManager ourInstance = new NetManager();
-
-    public static NetManager getInstance() {
-        return ourInstance;
-    }
-
-    private NetManager() {
-        this("http://wangsj.cn:8000/");
-//        this(new SharedPrefManager().getIpConfig(),"");
-    }
-
-    private NetManager(String url) {
-        super(BaseApplication.getContext(), url, "");
-    }
-
-    private NetManager(String url, String cer) {
-        super(BaseApplication.getContext(), url, cer);
-    }
+public class NetManager{
+//        extends HttpUtil {
+//    private static final NetManager ourInstance = new NetManager();
+//
+//    public static NetManager getInstance() {
+//        return ourInstance;
+//    }
+//
+//    private NetManager() {
+//        this("http://wangsj.cn:8000/");
+////        this(new SharedPrefManager().getIpConfig(),"");
+//    }
+//
+//    private NetManager(String url) {
+//        super(BaseApplication.getContext(), url, "",null);
+//    }
+//
+//    private NetManager(String url, String cer) {
+//        super(BaseApplication.getContext(), url, cer,null);
+//    }
 
 
 }
