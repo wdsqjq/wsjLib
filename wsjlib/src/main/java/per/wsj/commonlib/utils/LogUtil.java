612403f48cc457d9b49d2bc6fb337800042eb32d
shiju.wang <shiju.wang@wheatek.com>
2018-09-28 19:37:36 +0800

    optimize aliloading

--- wsjlib/src/main/java/per/wsj/commonlib/utils/LogUtil.java
+++ wsjlib/src/main/java/per/wsj/commonlib/utils/LogUtil.java
@@ -4,7 +4,7 @@ import android.util.Log;
 
 public class LogUtil {
     private static boolean DEBUG = true;
-    public static final String TAG = "AND_COMMUNITY";
+    public static final String TAG = "wsjLib";
 
     public static void LOGI(String tag, String msg) {
         if (DEBUG) {
