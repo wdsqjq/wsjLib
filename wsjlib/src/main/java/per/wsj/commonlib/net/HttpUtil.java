6d2769ec4c5ec4db0da0abe2a20c2611fc75f3b4
giswangsj <giswangsj@163.com>
2019-05-25 21:22:52 +0800

    optimize

--- wsjlib/src/main/java/per/wsj/commonlib/net/HttpUtil.java
+++ wsjlib/src/main/java/per/wsj/commonlib/net/HttpUtil.java
@@ -1,22 +1,15 @@
 package per.wsj.commonlib.net;
 
 import android.content.Context;
-
 import com.alibaba.fastjson.JSON;
-
 import org.json.JSONObject;
-
 import java.net.ConnectException;
 import java.net.SocketTimeoutException;
 import java.net.UnknownHostException;
 import java.util.ArrayList;
 import java.util.Map;
 import java.util.concurrent.TimeUnit;
-
 import javax.net.ssl.SSLHandshakeException;
-import javax.net.ssl.SSLSocketFactory;
-import javax.net.ssl.X509TrustManager;
-
 import io.reactivex.Observable;
 import io.reactivex.Observer;
 import io.reactivex.android.schedulers.AndroidSchedulers;
