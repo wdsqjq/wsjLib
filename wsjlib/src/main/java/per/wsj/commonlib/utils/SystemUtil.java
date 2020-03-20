b3e0c4386e88635754dd536c477ce4cf00df3682
shiju.wang <shiju.wang@wheatek.com>
2020-03-20 18:16:17 +0800

    add systemutil method

--- wsjlib/src/main/java/per/wsj/commonlib/utils/SystemUtil.java
+++ wsjlib/src/main/java/per/wsj/commonlib/utils/SystemUtil.java
@@ -2,6 +2,9 @@ package per.wsj.commonlib.utils;
 
 import android.app.Activity;
 import android.content.Context;
+import android.net.wifi.WifiInfo;
+import android.net.wifi.WifiManager;
+import android.provider.Settings;
 import android.telephony.TelephonyManager;
 
 import java.util.Locale;
@@ -19,7 +22,7 @@ public class SystemUtil {
     /**
      * 获取当前系统上的语言列表(Locale列表)
      *
-     * @return  语言列表
+     * @return 语言列表
      */
     public static Locale[] getSystemLanguageList() {
         return Locale.getAvailableLocales();
@@ -28,7 +31,7 @@ public class SystemUtil {
     /**
      * 获取当前手机系统版本号
      *
-     * @return  系统版本号
+     * @return 系统版本号
      */
     public static String getSystemVersion() {
         return android.os.Build.VERSION.RELEASE;
@@ -37,7 +40,7 @@ public class SystemUtil {
     /**
      * 获取手机型号
      *
-     * @return  手机型号
+     * @return 手机型号
      */
     public static String getSystemModel() {
         return android.os.Build.MODEL;
@@ -46,19 +49,57 @@ public class SystemUtil {
     /**
      * 获取手机厂商
      *
-     * @return  手机厂商
+     * @return 手机厂商
      */
     public static String getDeviceBrand() {
         return android.os.Build.BRAND;
     }
 
     /**
-     * 获取手机IMEI(需要“android.permission.READ_PHONE_STATE”权限)
+     * 获取ANDROID_ID
      *
-     * @return  手机IMEI
+     * @return
      */
-    public static String getIMEI(Context ctx) {
-        TelephonyManager tm = (TelephonyManager) ctx.getSystemService(Activity.TELEPHONY_SERVICE);
+    public static String getAndroid_Id(Context context) {
+        return Settings.System.getString(context.getContentResolver(), Settings.Secure.ANDROID_ID);
+    }
+
+    /**
+     * 获取mac地址
+     * 在获取MAC地址之前先判断当前WiFi状态，若开启了Wifi，则直接获取MAC地址，若没开启Wifi，则用代码开启Wifi，然后马上关闭，再获取MAC地址。
+     * <p>
+     * 权限：android.permission.ACCESS_WIFI_STATE/android.permission.CHANGE_WIFI_STATE/android.permission.WAKE_LOCK
+     *
+     * @param context
+     * @return
+     */
+    public String getMacAddress(Context context) {
+
+        String macAddress = null;
+        WifiManager wifiManager =
+                (WifiManager) context.getSystemService(Context.WIFI_SERVICE);
+        WifiInfo info = (null == wifiManager ? null : wifiManager.getConnectionInfo());
+
+        if (!wifiManager.isWifiEnabled()) {
+            //必须先打开，才能获取到MAC地址
+            wifiManager.setWifiEnabled(true);
+            wifiManager.setWifiEnabled(false);
+        }
+        if (null != info) {
+            macAddress = info.getMacAddress();
+        }
+        return macAddress;
+    }
+
+    /**
+     * 获取手机IMEI
+     * IMEI号（国际移动设备身份码）、IMSI号（国际移动设备识别码）这两个是有电话功能的移动设备才具有，也就是说某些没有电话功能的平板是获取不到IMEI和IMSI号的
+     * (需要动态申请“android.permission.READ_PHONE_STATE”权限)
+     *
+     * @return 手机IMEI
+     */
+    public static String getIMEI(Context context) {
+        TelephonyManager tm = (TelephonyManager) context.getSystemService(Activity.TELEPHONY_SERVICE);
         if (tm != null) {
             return tm.getDeviceId();
         }
