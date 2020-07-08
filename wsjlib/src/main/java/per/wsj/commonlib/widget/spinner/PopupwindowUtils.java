afc9fc4cc42ca1beb12f94ac9eb18550a19ca652
wangsj <giswangsj@163.com>
2020-07-09 00:10:04 +0800

    optimize popupwindow util

--- wsjlib/src/main/java/per/wsj/commonlib/widget/spinner/PopupwindowUtils.java
+++ wsjlib/src/main/java/per/wsj/commonlib/widget/spinner/PopupwindowUtils.java
@@ -6,6 +6,7 @@ import android.graphics.drawable.ColorDrawable;
 import android.view.Gravity;
 import android.view.LayoutInflater;
 import android.view.View;
+import android.view.ViewGroup;
 import android.widget.FrameLayout;
 import android.widget.LinearLayout;
 import android.widget.PopupWindow;
@@ -20,59 +21,25 @@ import per.wsj.commonlib.R;
 
 public class PopupwindowUtils {
 
-    private volatile static PopupwindowUtils mInstance;
-    private static PopupWindow popupWindow;
-
-    private PopupwindowUtils() {
-    }
-
-    public static PopupwindowUtils getInstance() {
-        if (mInstance == null) {
-            synchronized (PopupwindowUtils.class) {
-                if (mInstance == null) {
-                    mInstance = new PopupwindowUtils();
-                }
-            }
-        }
-        return mInstance;
-    }
-
-    /**
-     * 显示PopupWindow位置
-     *
-     * @param target
-     * @param popupWindow
-     */
-    public static void showPopupWindowLocation(View target, PopupWindow popupWindow, int gravity) {
-//        if (Build.VERSION.SDK_INT >= 24) {
-//            Rect visibleFrame = new Rect();
-//            target.getGlobalVisibleRect(visibleFrame);
-//            int height = target.getResources().getDisplayMetrics().heightPixels - visibleFrame.bottom;
-//            popupWindow.setHeight(height);
-//        }
-        popupWindow.showAsDropDown(target);
-
-    }
 
     /**
      * 显示菜单
      */
-    public <T> void showMenu(Context context, View target, List<T> datas, final OnPopupWindowListener<T> listener) {
+    public static <T> void showMenu(Context context, View target, List<T> datas, final OnPopupWindowListener<T> listener) {
         View view = LayoutInflater.from(context).inflate(R.layout.popupwindow_menu, null);
         int width = target.getWidth();
 //        popupWindow = new PopupWindow(view, FrameLayout.LayoutParams.WRAP_CONTENT,
 //                FrameLayout.LayoutParams.WRAP_CONTENT, true);
-        popupWindow = new PopupWindow(view, width, FrameLayout.LayoutParams.WRAP_CONTENT, true);
+        PopupWindow popupWindow = new PopupWindow(view, width, FrameLayout.LayoutParams.WRAP_CONTENT, true);
 
-        LinearLayout rlParent = view.findViewById(R.id.llParent);
+        ViewGroup rlParent = view.findViewById(R.id.llParent);
         rlParent.setOnClickListener(view1 -> popupWindow.dismiss());
 
         RecyclerView recyclerView = view.findViewById(R.id.rvMenu);
 
         recyclerView.setLayoutManager(new LinearLayoutManager(context));
 
-        recyclerView.setAdapter(new CommonAdapter<T>(context, R.layout.popupwindow_menu_item,
-                datas) {
+        recyclerView.setAdapter(new CommonAdapter<T>(context, R.layout.popupwindow_menu_item, datas) {
             @Override
             public void convert(RecyclerHolder holder, T item, int position) {
                 if (listener != null) {
@@ -96,17 +63,24 @@ public class PopupwindowUtils {
             // 拦截后 PopupWindow的onTouchEvent不被调用，这样点击外部区域无法dismiss
             return false;
         });
-        ColorDrawable drawable = new ColorDrawable(Color.TRANSPARENT);
-        popupWindow.setBackgroundDrawable(drawable);
+//        ColorDrawable drawable = new ColorDrawable(Color.TRANSPARENT);
+//        popupWindow.setBackgroundDrawable(drawable);
         showPopupWindowLocation(target, popupWindow, Gravity.NO_GRAVITY);
     }
 
-    public void dismissPopWindow() {
-        if (popupWindow == null) {
-            return;
-        }
-        popupWindow.dismiss();
-        popupWindow = null;
+    /**
+     * 显示PopupWindow位置
+     *
+     * @param target
+     * @param popupWindow
+     */
+    public static void showPopupWindowLocation(View target, PopupWindow popupWindow, int gravity) {
+//        if (Build.VERSION.SDK_INT >= 24) {
+//            Rect visibleFrame = new Rect();
+//            target.getGlobalVisibleRect(visibleFrame);
+//            int height = target.getResources().getDisplayMetrics().heightPixels - visibleFrame.bottom;
+//            popupWindow.setHeight(height);
+//        }
+        popupWindow.showAsDropDown(target);
     }
-
 }
