47d8ff7586f803837338a5d0c6c6477cde93cea6
shiju.wang <shiju.wang@wheatek.com>
2020-11-10 17:24:13 +0800

    403 forbidden

--- wsjlib/src/main/java/per/wsj/commonlib/net/HttpCallback2.java
+++ wsjlib/src/main/java/per/wsj/commonlib/net/HttpCallback2.java
@@ -35,13 +35,15 @@ public abstract class HttpCallback2<T> implements Observer<T>, CallBack<T> {
                 onSuccess(null, code, "success");
             } else if (code.equals("307")) {
                 // Response{protocol=http/1.1, code=307, message=Temporary Redirect, url=https://api.github.com/user/starred/FlowCI/flow-platform-x}
-                onError(null, "该仓库有点小异常，请在web端操作该仓库","307");
+                onError(null, "该仓库有点小异常，请在web端操作该仓库", "307");
             } else if (code.equals("401")) {
-                onError(null, "请重新登录","401");
+                onError(null, "请重新登录", code);
             } else if (code.equals("404")) {
-                onError(null, "没有记录","404");
+                onError(null, "没有记录", code);
+            } else if (code.equals("403")) {
+                onError(null, "你的github账号邮箱验证已过期，请登录github进行邮箱验证", code);
             } else {
-                onError(null, "请求失败","0000");
+                onError(null, "请求失败", "0000");
             }
         } catch (Exception e) {
             onError(e);
@@ -51,15 +53,15 @@ public abstract class HttpCallback2<T> implements Observer<T>, CallBack<T> {
     @Override
     public void onError(Throwable e) {
         if (e instanceof SocketTimeoutException) {
-            onError(e, "请求超时,请重试","0000");
+            onError(e, "请求超时,请重试", "0000");
         } else if (e instanceof HttpException
                 || e instanceof ConnectException
                 || e instanceof UnknownHostException) {
-            onError(e, "网络错误，请确保网络通畅","0000");
+            onError(e, "网络错误，请确保网络通畅", "0000");
         } else if (e instanceof SSLHandshakeException) {
-            onError(e, "网络异常，如使用网络代理，请关闭后重试","0000");
+            onError(e, "网络异常，如使用网络代理，请关闭后重试", "0000");
         } else {
-            onError(e, e.toString(),"0000");
+            onError(e, e.toString(), "0000");
         }
         onFinished();
     }
