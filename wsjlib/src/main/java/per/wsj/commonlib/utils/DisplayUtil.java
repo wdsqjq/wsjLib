e8bcbe83cbbda287afa1ee7ec910a84093472302
wsj1024 <wangsj1024@qq.com>
2021-07-24 10:41:46 +0800

    optimize displayUtil

--- wsjlib/src/main/java/per/wsj/commonlib/utils/DisplayUtil.java
+++ wsjlib/src/main/java/per/wsj/commonlib/utils/DisplayUtil.java
@@ -7,12 +7,14 @@ import android.graphics.Bitmap;
 import android.graphics.Color;
 import android.graphics.Point;
 import android.os.Build;
+import android.provider.Settings;
 import android.util.TypedValue;
 import android.view.View;
 import android.view.Window;
 import android.view.WindowManager;
 
 import java.lang.reflect.Field;
+import java.lang.reflect.Method;
 
 /**
  * Created by dell 王世举 on 2017/9/3 11:20.
@@ -238,5 +240,59 @@ public class DisplayUtil {
         Bitmap bp = Bitmap.createBitmap(bmp, 0, statusBarHeight, width, height - statusBarHeight);
         view.destroyDrawingCache();
         return bp;
+    }
+
+    /**
+     * 判断手机底部是否支持导航栏显示
+     * @param context
+     * @return
+     */
+    public static boolean isNavigationBarShowing(Context context) {
+        boolean haveNavigationBar = checkDeviceHasNavigationBar(context);
+        if (haveNavigationBar) {
+            if (Build.VERSION.SDK_INT >= 17) {
+                String brand = Build.BRAND;
+                String mDeviceInfo;
+                if (brand.equalsIgnoreCase("HUAWEI")) {
+                    mDeviceInfo = "navigationbar_is_min";
+                } else if (brand.equalsIgnoreCase("XIAOMI")) {
+                    mDeviceInfo = "force_fsg_nav_bar";
+                } else if (brand.equalsIgnoreCase("VIVO")) {
+                    mDeviceInfo = "navigation_gesture_on";
+                } else if (brand.equalsIgnoreCase("OPPO")) {
+                    mDeviceInfo = "navigation_gesture_on";
+                } else {
+                    mDeviceInfo = "navigationbar_is_min";
+                }
+
+                if (Settings.Global.getInt(context.getContentResolver(), mDeviceInfo, 0) == 0) {
+                    return true;
+                }
+            }
+        }
+        return false;
+    }
+
+
+    private static boolean checkDeviceHasNavigationBar(Context context) {
+        boolean hasNavigationBar = false;
+        Resources rs = context.getResources();
+        int id = rs.getIdentifier("config_showNavigationBar", "bool", "android");
+        if (id > 0) {
+            hasNavigationBar = rs.getBoolean(id);
+        }
+        try {
+            Class systemPropertiesClass = Class.forName("android.os.SystemProperties");
+            Method m = systemPropertiesClass.getMethod("get", String.class);
+            String navBarOverride = (String) m.invoke(systemPropertiesClass, "qemu.hw.mainkeys");
+            if ("1".equals(navBarOverride)) {
+                hasNavigationBar = false;
+            } else if ("0".equals(navBarOverride)) {
+                hasNavigationBar = true;
+            }
+        } catch (Exception e) {
+
+        }
+        return hasNavigationBar;
     }
 }
